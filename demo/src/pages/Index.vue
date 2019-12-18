b977124021016125087008df63c6733f593f2c68
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-12-18 07:37:13 -0700

    chore(demo): updated

--- demo/src/pages/Index.vue
+++ demo/src/pages/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <hero>
     <q-markdown :src="markdown" toc @data="onToc" />
-    <component-api
+    <json-api-viewer
       title="QThemeMixin API"
       :json="json"
       type="Vue Mixin"
